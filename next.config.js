e67df9103c0bfd04634c9addda78861ddf08a039
Jonny Thaw <hi@jthaw.me>
2021-02-08 12:58:09 +0000

    fix: removed pwa

--- next.config.js
+++ next.config.js
@@ -1,18 +1,33 @@
 const path = require("path");
 
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+// const withPWA = require("next-pwa");
+// const runtimeCaching = require("next-pwa/cache");
 
-module.exports = withPWA({
+// module.exports = withPWA({
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, "styles")],
+//     prependData: `@import "common.scss";`,
+//   },
+
+//   pwa: {
+//     disable: process.env.NODE_ENV === "development",
+//     dest: "public",
+//     runtimeCaching,
+//   },
+
+//   webpack(config) {
+//     config.module.rules.push({
+//       test: /\.svg$/,
+//       use: ["@svgr/webpack", "url-loader"],
+//     });
+
+//     return config;
+//   },
+// });
+module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
     prependData: `@import "common.scss";`,
-  },
-
-  pwa: {
-    disable: process.env.NODE_ENV === "development",
-    dest: "public",
-    runtimeCaching,
   },
 
   webpack(config) {
@@ -23,4 +38,4 @@ module.exports = withPWA({
 
     return config;
   },
-});
+};
